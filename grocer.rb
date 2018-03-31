88cd871e281ba48181d531bee9b67228b02e2bd1
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 18:26:43 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -1,11 +1,26 @@
 def consolidate_cart(cart)
   hash = {}
+  pre_item = nil
+  count = 1
   
   cart.each do |item|
+    if pre_item == nil || pre_item != item
+      count = 1
+      pre_item = item
+    else  
+      count += 1
+    end
+    
     item.each do |name, data|
       data.each do |attribute, value|
-        hash[name][attribute] = value
+        if hash[name] == nil
+          hash[name] = {attribute => value}
+        else
+          hash[name][attribute] = value
+        end
       end
+      
+      hash[name][:count] = count
     end
   end
   
